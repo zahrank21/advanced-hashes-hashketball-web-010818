832065bd21d3f210c230e1662fbb8f201062cea4
zahrank21 <zahrank@gmail.com>
2018-01-08 07:19:03 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,8 +117,8 @@ def game_hash
   }
 }
 end
-#access both :home->:players and :away->:players 
-#both players are arrays...find method that iterates through that array and returns :points 
+#access both :home->:players and :away->:players
+#both players are arrays...find method that iterates through that array and returns :points
 #must tell program to find it with a name(string) input
 def players
   home_players = game_hash[:home][:players]
@@ -130,61 +130,52 @@ end
 def num_points_scored(name)
   find_player = players.find {|player| player.fetch(:player_name) == name }
   find_player.fetch(:points)
-end 
+end
 
 num_points_scored("Brendan Haywood")
 
 def shoe_size(name)
   find_shoe = players.find {|player| player.fetch(:player_name) == name }
   find_shoe.fetch(:shoe)
-end 
+end
 
 shoe_size("Ben Gordon")
 
-def team_stats 
+def team_stats
   home_team = game_hash.values_at(:home)
   away_team = game_hash.values_at(:away)
   total_team_stats = home_team + away_team
   total_team_stats
-end 
-team_stats 
+end
+team_stats
 
 def team_colors(team_name)
   find_team = team_stats.find {|team| team.fetch(:team_name) == team_name}
   find_team.fetch(:colors)
-end 
+end
 team_colors("Brooklyn Nets")
 
-def team_names 
+def team_names
   team_stats.collect do |team|
     team[:team_name]
-  end 
-end 
-team_names 
+  end
+end
+team_names
 
 def player_numbers(team_name)
   find_numbers = team_stats.find {|team| team.fetch(:team_name) == team_name}
-  find_numbers[:players].collect do |player| 
+  find_numbers[:players].collect do |player|
     player[:number]
-  end 
-end 
+  end
+end
 player_numbers("Brooklyn Nets")
 
 # def player_stats(name)
 #   stats = players.find {|stat| stat.fetch(:player_name) == name}
 #   stats
-# end 
+# end
 
 def player_stats(player_name)
  stats= players.find {|player| player.fetch(:player_name) == player_name}
   stats.delete_if {|info, string| info == :player_name}
 end
-
-
-
-
-
-
-
-
-
